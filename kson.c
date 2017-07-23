dad99f9c8a03b6e31c0ce10802446e76668708a3
Capitan-KoLiBer <koliberr136a1@gmail.com>
2017-07-23 19:45:11 +0430

    Add files via upload

--- kson.c
+++ kson.c
@@ -2,6 +2,7 @@
 
 Kson *kson_new(){
     Kson *kson = malloc(sizeof(Kson));
+    kson->next = NULL;
     return kson;
 }
 
@@ -51,8 +52,7 @@ void kson_string_pack(char **kson_string){
     strcpy(*kson_string,string);
 }
 
-void kson_parse(char *kson_pack , Kson *kson){
-    Kson *next_kson = kson;
+void kson_parse(char *kson_pack , Kson **kson){
     char *key = malloc(sizeof(char)*strlen(kson_pack));
     char *value = malloc(sizeof(char)*strlen(kson_pack));
     int i_cursor = 0;
@@ -92,10 +92,11 @@ void kson_parse(char *kson_pack , Kson *kson){
                     realloc(value,sizeof(char)*strlen(value));
                     kson_string_parse(&key);
                     kson_string_parse(&value);
-                    next_kson->key = key;
-                    next_kson->value = value;
-                    next_kson->next = malloc(sizeof(Kson));
-                    next_kson = next_kson->next;
+                    Kson *new_kson = malloc(sizeof(Kson));
+                    new_kson->key = key;
+                    new_kson->value = value;
+                    new_kson->next = *kson;
+                    *kson = new_kson;
                     key = malloc(sizeof(char)*strlen(kson_pack));
                     value = malloc(sizeof(char)*strlen(kson_pack));
                 }else{
@@ -107,6 +108,8 @@ void kson_parse(char *kson_pack , Kson *kson){
 
         }
     }
+    free(key);
+    free(value);
 }
 
 void kson_pack(Kson *kson , char **kson_pack){
@@ -163,8 +166,8 @@ void kson_put(struct Kson **kson , const char *key , const char *value){
         Kson *kson_item = malloc(sizeof(Kson));
         char *item_key = malloc(sizeof(char)*strlen(key));
         char *item_value = malloc(sizeof(char)*strlen(value));
-        strcat(item_key,key);
-        strcat(item_value,value);
+        strcpy(item_key,key);
+        strcpy(item_value,value);
         kson_item->key = item_key;
         kson_item->value = item_value;
         kson_item->next = *kson;
@@ -196,4 +199,147 @@ void kson_remove(struct Kson **kson , const char *key){
         (*kson) = swap;
     } while((*kson)->next != NULL);
     *kson = first_kson;
+}
+
+Kson_Iterator* kson_iterator(struct Kson *kson){
+    Kson_Iterator *kson_iterator = malloc(sizeof(Kson_Iterator));
+    kson_iterator->kson = kson;
+    return kson_iterator;
+}
+
+char *kson_keys_iterate(struct Kson *kson, struct Kson_Iterator **kson_iterator){
+    char *return_key = (*kson_iterator)->kson->key;
+    if((*kson_iterator)->kson == NULL){
+        (*kson_iterator)->kson = kson;
+    }
+    (*kson_iterator)->kson = (*kson_iterator)->kson->next;
+    return return_key;
+}
+
+Kson_Array *kson_array_new(){
+    Kson_Array *kson_array = malloc(sizeof(Kson_Array));
+    kson_array->next = NULL;
+    return kson_array;
+}
+
+void kson_array_free(Kson_Array *kson_array){
+    Kson_Array *swap = NULL;
+    do{
+        swap = kson_array->next;
+        free(kson_array->value);
+        free(kson_array);
+        kson_array = swap;
+    } while(kson_array->next != NULL);
+}
+
+void kson_array_parse(const char *kson_array_pack , struct Kson_Array **kson_array){
+    char *value = malloc(sizeof(char)*strlen(kson_array_pack));
+    int i_cursor = 0;
+    int is_inner_value = 0; // -> 0 , 1 => 0 -> when not in value , 1 -> when in value
+    for(int cursor = 0 ; cursor < strlen(kson_array_pack) ; cursor++){
+        if(is_inner_value == 0){
+            if(kson_array_pack[cursor] == '\"' && kson_array_pack[cursor-1] != '\\'){
+                is_inner_value = 1;
+            }
+        }else{
+            if(kson_array_pack[cursor] == '\"' && kson_array_pack[cursor-1] != '\\'){
+                is_inner_value = 0;
+                *(value+i_cursor) = '\0';
+                i_cursor = 0;
+                realloc(value,sizeof(char)*strlen(value));
+                kson_string_parse(&value);
+                Kson_Array *new_array = malloc(sizeof(Kson_Array));
+                new_array->value = value;
+                new_array->next = *kson_array;
+                *kson_array = new_array;
+                value = malloc(sizeof(char)*strlen(kson_array_pack));
+            }else{
+                *(value+i_cursor) = kson_array_pack[cursor];
+                i_cursor++;
+            }
+        }
+    }
+    free(value);
+}
+
+void kson_array_pack(struct Kson_Array *kson_array , char **kson_array_pack){
+    *kson_array_pack = malloc(2048*sizeof(char));
+    strcat(*kson_array_pack,"[");
+    Kson_Array *swap = NULL;
+    do{
+        kson_string_pack(&kson_array->value);
+        swap = kson_array->next;
+        strcat(*kson_array_pack,"\"");
+        strcat(*kson_array_pack,kson_array->value);
+        strcat(*kson_array_pack,"\"");
+        if(kson_array->next->next != NULL){
+            strcat(*kson_array_pack,",");
+        }
+        kson_array = swap;
+    } while(kson_array->next != NULL);
+    strcat(*kson_array_pack,"]");
+}
+
+void kson_array_get(struct Kson_Array *kson_array , const int index , char **value){
+    Kson_Array *swap = NULL;
+    for(int cursor = 0 ; cursor < index ; cursor ++){
+        swap = kson_array->next;
+        kson_array = swap;
+    }
+    if(kson_array->value != NULL){
+        (*value) = malloc(sizeof(char)*strlen(kson_array->value));
+        strcpy(*value,kson_array->value);
+    }
+}
+
+void kson_array_add(struct Kson_Array **kson_array , const char *value){
+    Kson_Array *kson_array_item = malloc(sizeof(Kson_Array));
+    char *item_value = malloc(sizeof(char)*strlen(value));
+    strcpy(item_value,value);
+    kson_array_item->value = item_value;
+    kson_array_item->next = *kson_array;
+    *kson_array = kson_array_item;
+}
+
+void kson_array_set(struct Kson_Array *kson_array , const int index , const char *value){
+    Kson_Array *swap = NULL;
+    for(int cursor = 0 ; cursor < index ; cursor ++){
+        swap = kson_array->next;
+        kson_array = swap;
+    }
+    if(kson_array != NULL){
+        free(kson_array->value);
+        kson_array->value = malloc(strlen(value)*sizeof(char));
+        strcpy(kson_array->value,value);
+    }
+}
+
+void kson_array_remove(struct Kson_Array **kson_array , const int index){
+    Kson_Array *swap = NULL;
+    Kson_Array *first_kson_array = *kson_array;
+    Kson_Array *before_kson_array = NULL;
+    for(int cursor = 0 ; cursor < index ; cursor++){
+        swap = (*kson_array)->next;
+        before_kson_array = (*kson_array);
+        (*kson_array) = swap;
+    }
+    if(before_kson_array != NULL){
+        before_kson_array->next = (*kson_array)->next;
+        free((*kson_array)->value);
+        free(*kson_array);
+    }
+    *kson_array = first_kson_array;
+}
+
+int kson_array_length(struct Kson_Array *kson_array){
+    Kson_Array *swap = NULL;
+    int length = 0;
+    do{
+        swap = kson_array->next;
+        free(kson_array->value);
+        free(kson_array);
+        kson_array = swap;
+        length++;
+    } while(kson_array->next != NULL);
+    return length;
 }
--- kson.h
+++ kson.h
@@ -7,12 +7,19 @@ typedef struct Kson{
     char *value;
     struct Kson *next;
 } Kson;
+typedef struct Kson_Iterator{
+    struct Kson *kson;
+} Kson_Iterator;
+typedef struct Kson_Array{
+    char *value;
+    struct Kson_Array *next;
+} Kson_Array;
 
 Kson *kson_new();
 
 void kson_free(Kson *kson);
 
-void kson_parse(char *kson_pack , struct Kson *kson);
+void kson_parse(char *kson_pack , struct Kson **kson);
 
 void kson_pack(struct Kson *kson , char **kson_pack);
 
@@ -21,3 +28,25 @@ void kson_get(struct Kson *kson , const char *key,char **value);
 void kson_put(struct Kson **kson , const char *key , const char *value);
 
 void kson_remove(struct Kson **kson , const char *key);
+
+Kson_Iterator* kson_iterator(struct Kson *kson);
+
+char *kson_keys_iterate(struct Kson *kson, struct Kson_Iterator **kson_iterator);
+
+Kson_Array *kson_array_new();
+
+void kson_array_free(Kson_Array *kson_array);
+
+void kson_array_parse(const char *kson_array_pack , struct Kson_Array **kson_array);
+
+void kson_array_pack(struct Kson_Array *kson_array , char **kson_array_pack);
+
+void kson_array_get(struct Kson_Array *kson_array , const int index , char **value);
+
+void kson_array_add(struct Kson_Array **kson_array , const char *value);
+
+void kson_array_set(struct Kson_Array *kson_array , const int index , const char *value);
+
+void kson_array_remove(struct Kson_Array **kson_array , const int index);
+
+int kson_array_length(struct Kson_Array *kson_array);
